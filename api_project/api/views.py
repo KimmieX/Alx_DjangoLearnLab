dc9f722e594cdd5fec0b44e58af336d7d2acce20
Kimberly <hatsukimberly5@gmail.com>
2025-08-26 22:51:02 +0000

    update views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer
-
+from rest_framework import generics
 
 # Create your views here.
 class BookList(ListAPIView):
