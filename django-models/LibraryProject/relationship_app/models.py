fa66f8c2f29ca4d47b81110a4da5fe1ed01b19a5
Kimberly <hatsukimberly5@gmail.com>
2025-08-26 13:34:38 +0000

    adding templates

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -18,6 +21,13 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
+    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
 
 
 
