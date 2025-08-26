c506a605c9b17b4837923cea4e01647cab64c8e9
Kimberly <hatsukimberly5@gmail.com>
2025-08-26 13:00:57 +0000

    updating template

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.shortcuts import render
 from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
@@ -7,12 +8,15 @@ from django.contrib.auth.forms import UserCreationForm
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+
 
 def list_books(request):
     books = Book.objects.select_related('author').all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
 
 def register(request):
     if request.method == 'POST':
