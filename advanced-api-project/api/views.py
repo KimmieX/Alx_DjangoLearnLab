47f621f37d7dce1d81db2747693b59587116f42b
Kimberly <hatsukimberly5@gmail.com>
2025-08-27 17:46:49 +0000

    update package

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,5 @@
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import render
 from rest_framework import generics, permissions
 from .models import Book
@@ -11,6 +13,17 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.AllowAny]  # Public read access
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    filterset_fields = ['title', 'author', 'publication_year']
+    
+    search_fields = ['title', 'author']
+    
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title'] 
+
+
+
 
 # Retrieve a single book
 class BookDetailView(generics.RetrieveAPIView):
