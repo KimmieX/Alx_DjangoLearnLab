f889088d093c176c3e18b7f38631f3774bd2fd7f
Kimberly <hatsukimberly5@gmail.com>
2025-08-27 19:31:47 +0000

    creating templates

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        request.user.email = request.POST.get('email')
+        request.user.save()
+    return render(request, 'profile.html', {'user': request.user})
+
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.auth import views as auth_views
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
