b6907c1c43cafd594d16b6b5fbf3a4dc00d30699
Kimberly <hatsukimberly5@gmail.com>
2025-08-29 17:14:47 +0000

    update views and urls

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
 from .views import user_feed
+from .views import FeedView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
@@ -10,5 +11,6 @@ router.register(r'comments', CommentViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('feed/', user_feed),
-
+    path('feed/', FeedView.as_view(), name='user-feed'),
 ]
+
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -8,6 +8,7 @@ from rest_framework.response import Response
 from .models import Post
 from .serializers import PostSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, permissions
 
 
 # Create your views here.
@@ -40,3 +41,12 @@ def user_feed(request):
     posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
+
+class FeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        following_users = self.request.user.following.all()
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
+
