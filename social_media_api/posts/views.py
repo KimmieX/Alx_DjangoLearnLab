ffc9124f584682d5acfd048e75644f3e7d8cc69f
Kimberly <hatsukimberly5@gmail.com>
2025-08-29 17:30:38 +0000

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -13,9 +13,9 @@ from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Post, Like
-from notifications.models import Notification
+from user_notifications.models import Notification
 from django.contrib.contenttypes.models import ContentType
-
+from django.contrib.contenttypes.models import ContentType
 
 # Create your views here.
 class IsOwnerOrReadOnly(permissions.BasePermission):
@@ -60,7 +60,7 @@ class LikePostView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk):
-        post = Post.objects.get(pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         like, created = Like.objects.get_or_create(user=request.user, post=post)
         if created:
             Notification.objects.create(
@@ -76,12 +76,13 @@ class UnlikePostView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def post(self, request, pk):
-        post = Post.objects.get(pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         try:
             like = Like.objects.get(user=request.user, post=post)
             like.delete()
             return Response({'status': 'unliked'}, status=status.HTTP_200_OK)
         except Like.DoesNotExist:
             return Response({'status': 'not liked'}, status=status.HTTP_400_BAD_REQUEST)
+
 
 
